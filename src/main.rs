bdf9b120e2b7e1a43da1649c7b7944f919a51188
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2017-12-27 11:50:50 +0900

    image file output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate image;
+
+use std::env;
+use std::fs::File;
+use std::path::Path;
+
+use image::GenericImage;
+
 fn main() {
-    println!("Hello, world!");
+    let file = if env::args().count() == 2 {
+        env::args().nth(1).unwrap()
+    } else {
+        panic!("Please enter a file")
+    };
+
+    let im = image::open(&Path::new(&file)).unwrap();
+    println!("dimensions {:?}", im.dimensions());
+    println!("{:?}", im.color());
+    let fout = &mut File::create(&Path::new(&format!("fix_{}", file))).unwrap();
+    im.save(fout, image::BMP).unwrap();
 }
